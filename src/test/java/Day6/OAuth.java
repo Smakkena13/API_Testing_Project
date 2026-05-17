10f3516eb56b0652a53f215a0f0e25ab452890e0
Smakkena <msuvarnakumari13@gmail.com>
2026-05-17 15:43:12 +0530

    POJO Classes

--- src/test/java/Day6/OAuth.java
+++ src/test/java/Day6/OAuth.java
@@ -1,7 +1,12 @@
 package Day6;
 
+import Day6.Pojo.EntireData;
+import Day6.Pojo.Mobile;
+import Day6.Pojo.WebAutomation;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -20,8 +25,37 @@ public class OAuth {
         System.out.println("token: "+token);
 
         //use same token to get the book
-        given().log().all().queryParam("access_token",token)
+        Day6.Pojo.EntireData ed =given().log().all().queryParam("access_token",token)
                 .when().get("/oauthapi/getCourseDetails")
-                .then().log().all().assertThat().statusCode(401);
+                .as(Day6.Pojo.EntireData.class);
+
+        System.out.println(ed.getLinkedIn());
+        System.out.println(ed.getInstructor());
+        //
+        System.out.println(ed.getCourses().getWebAutomation().get(0).getCourseTitle());
+        //get the procator price
+        List<WebAutomation> wa=ed.getCourses().getWebAutomation();
+        for(WebAutomation w:wa){
+            if(w.getCourseTitle().equalsIgnoreCase("Protractor")){
+                System.out.println(w.getPrice());
+            }
+        }
+        //get the mobile title
+        List<Mobile> mb=ed.getCourses().getMobile();
+        for(Mobile m:mb){
+            System.out.println(m.getCourseTitle());
+        }
+
+        //sum all the prices in WebAutomation
+        List<WebAutomation> web=ed.getCourses().getWebAutomation();
+        int sum=0;
+        for(WebAutomation w:web){
+            String price=w.getPrice();
+            sum=sum+Integer.parseInt(price);
+        }
+        System.out.println("sum: "+sum);
+
+
+
     }
 }
